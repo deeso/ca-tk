c6465f2cfddc30e2377076009a6cd039ce300adc
dso <adam.pridgen.phd@gmail.com>
2020-07-30 22:33:12 -0500

    loads meta data and test passes

--- src/ca_tk/linux/core.py
+++ src/ca_tk/linux/core.py
@@ -1,7 +1,7 @@
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import NoteSection
 from .core_structures_x86 import *
-from memory_analysis.manager import Manager
+from ma_tk.manager import Manager
 from .. load import Elf
 
 # Steps to mapping in files
@@ -47,55 +47,100 @@ class ElfCore(object):
         self.elf = None
         self.source = None
         self.load_elf(core_filename, core_data, inmemory_only, zip_filename)
-
+        self.get_meta()
         
         self.physical_ranges = []
         self.virtual_ranges = []
 
         # parse out each relevant program hdr and segment
-        self.elf_sections = [i for i in self.elf.iter_sections()]
-        self.elf_segments = [i for i in self.elf.iter_segments()]
-        self.pt_loads = self.get_pt_loads()
-        self.notes = self.get_notes()
 
         self.page_size = 4096
         # map pages to a specific range
         self.virtual_cache = dict()
         self.mgr = Manager()
 
-    def get_notes(self):
-        if hasattr(self, 'notes'):
-            return self.notes
+    def get_meta(self):
+        self.get_segments()
+        self.get_sections()
+        self.get_pt_notes()
+        self.get_pt_loads()
+        self.get_notes()
+        self.get_prstatus_notes()
+        self.get_fpregset_notes()
+        self.get_prpsinfo_notes()
+        self.get_taskstruct_notes()
+        self.get_siginfo_notes()
+        self.get_auxv_notes()
+        self.get_xstate_notes()
+        self.get_file_notes()
+
+    def get_segments(self):
+        if not hasattr(self, 'segments'):
+            self.segments = [i for i in self.elf.iter_segments()]
+        return self.segments
         
-        pt_note = self.get_pt_notes()[0]
-        self.notes = [n for n in pt_note.iter_notes()]   
+    def get_sections(self):
+        if not hasattr(self, 'sections'):
+            self.elf_sections = [i for i in self.elf.iter_sections()]
+        return self.sections
+
+    def get_notes(self):
+        if not hasattr(self, 'notes'):
+            pt_note = self.get_pt_notes()[0]
+            self.notes = [n for n in pt_note.iter_notes()]
+        return self.note 
 
     def get_pt_loads(self):
-        return [i for i in self.elf_segments if i.header.p_type == 'PT_LOAD']
+        if not hasattr(self, 'pt_loads'):
+            self.pt_loads = [i for i in self.elf_segments if i.header.p_type == 'PT_LOAD']
+        return self.pt_loads
 
     def get_pt_notes(self):
-        return [i for i in self.elf_segments if i.header.p_type == 'PT_NOTE']
+        if not hasattr(self, 'pt_notes'):
+            self.pt_notes = [i for i in self.elf_segments if i.header.p_type == 'PT_NOTE']
+        return self.pt_notes
 
     def get_prstatus_notes(self, notes):
-        return [i for i in notes if i['n_type'] == 'NT_PRSTATUS' or i['n_type'] == 1]
+        notes = self.notes
+        if not hasattr(self, 'nt_prstatus'):
+            self.nt_prstatus = [i for i in notes if i['n_type'] == 'NT_PRSTATUS' or i['n_type'] == 1]
+        return self.nt_prstatus
 
-    def get_fpregset_notes(self, notes):
-        return [i for i in notes if i['n_type'] == 'NT_FPREGSET' or i['n_type'] == 2]
+    def get_fpregset_notes(self):
+        notes = self.notes
+        if not hasattr(self, 'nt_fpregset'):
+            self.nt_fpregset = [i for i in notes if i['n_type'] == 'NT_FPREGSET' or i['n_type'] == 2]
+        return self.nt_fpregset
 
-    def get_prpsinfo_notes(self, notes):
-        return [i for i in notes if i['n_type'] == 'NT_PRPSINFO' or i['n_type'] == 3]
+    def get_prpsinfo_notes(self):
+        notes = self.notes
+        if not hasattr(self, 'nt_prpsinfo'):
+            self.nt_prpsinfo = [i for i in notes if i['n_type'] == 'NT_PRPSINFO' or i['n_type'] == 3]
+        return self.nt_prpsinfo
 
-    def get_taskstruct_notes(self, notes):
-        return [i for i in notes if i['n_type'] == 'NT_TASKSTRUCT' or i['n_type'] == 4]
+    def get_taskstruct_notes(self):
+        notes = self.notes
+        if not hasattr(self, 'nt_taskstruct'):
+            self.nt_taskstruct = [i for i in notes if i['n_type'] == 'NT_TASKSTRUCT' or i['n_type'] == 4]
+        return self.nt_taskstruct
 
-    def get_auxv_notes(self, notes):
-        return [i for i in notes if i['n_type'] == 'NT_AUXV' or i['n_type'] == 4]
+    def get_auxv_notes(self):
+        notes = self.notes
+        if not hasattr(self, 'nt_auxv'):
+            self.nt_auxv = [i for i in notes if i['n_type'] == 'NT_AUXV' or i['n_type'] == 4]
+        return self.nt_auxv
 
-    def get_siginfo_notes(self, notes):
-        return [i for i in notes if i['n_type'] == 'NT_SIGINFO' or i['n_type'] == 0x53494749]
+    def get_siginfo_notes(self):
+        notes = self.notes
+        if not hasattr(self, 'nt_siginfo'):
+            self.nt_siginfo = [i for i in notes if i['n_type'] == 'NT_SIGINFO' or i['n_type'] == 0x53494749]
+        return self.nt_siginfo
 
-    def get_file_notes(self, notes):
-        return [i for i in notes if i['n_type'] == 'NT_FILE' or i['n_type'] == 0x46494c45]
+    def get_file_notes(self):
+        notes = self.notes
+        if not hasattr(self, 'nt_file'):
+            self.nt_file = [i for i in notes if i['n_type'] == 'NT_FILE' or i['n_type'] == 0x46494c45]
+        return self.nt_file
 
     def get_xstate_notes(self, notes):
         return [i for i in notes if i['n_type'] == 'NT_X86_XSTATE' or i['n_type'] == 0x202]        
---
+++ tests/testing/basic_elf_core_test.py
@@ -0,0 +1,57 @@
+import zipfile
+import tempfile
+import io
+import os
+import unittest
+from ca_tk.linux.core import ElfCore
+from ca_tk.load import Elf
+
+class TestManager(unittest.TestCase):
+    THIS_MODULE = os.path.realpath(__file__)
+    THIS_DIR = os.path.split(THIS_MODULE)[0]
+    SAMPLE_DIR = THIS_DIR + '/../../sample/'
+    ZIP_SAMPLE = SAMPLE_DIR + 'ipython-core-all.31971.zip'
+    TMP_FILE = None
+    TMP_NAME = None
+    DATA = None
+    def setUp(self):
+        # setup our tmp file
+        self.TMP_FILE = tempfile.NamedTemporaryFile()
+        zf = zipfile.ZipFile(self.ZIP_SAMPLE)
+        self.DATA = zf.read(zf.namelist()[0])
+        self.TMP_FILE.write(self.DATA)
+        self.TMP_FILE.flush()
+        self.TMP_FILE.seek(0)
+        self.TMP_NAME = self.TMP_FILE.name
+
+    def test_elf_parsing(self):
+        # from file is really slow, analysis needs to be
+        # in memory, or prepare to sit for a lot longer
+        assertTrue = self.assertTrue
+        fd, ef = Elf.from_zip(self.ZIP_SAMPLE, inmemory=True)
+
+        sections = [i for i in ef.iter_sections()]
+        assertTrue(len(sections) == 132)
+        assertTrue(sorted({i.header.sh_type for i in sections}) == ['SHT_NOTE', 'SHT_NULL', 'SHT_PROGBITS', 'SHT_STRTAB'])
+
+        segments = [i for i in ef.iter_segments()]
+        assertTrue(len(segments) == 130)
+        assertTrue(sorted({i.header.p_type for i in segments}) == ['PT_LOAD', 'PT_NOTE'])
+
+        pt_loads = [i for i in segments if i.header.p_type == 'PT_LOAD']
+        assertTrue(len(pt_loads) == 129)
+        pt_notes = [i for i in segments if i.header.p_type == 'PT_NOTE']
+        pt_note = pt_notes[0]
+        notes = [n for n in pt_notes[0].iter_notes()]
+        assertTrue(len(notes) == 15)
+
+        aux_notes = [n for n in pt_note.iter_notes() if n['n_type'] == 'NT_AUXV']
+        assertTrue(len(aux_notes) == 1)
+        file_notes = [n for n in pt_note.iter_notes() if n['n_type'] == 'NT_FILE']
+        assertTrue(len(file_notes) == 1)
+
+    def test_core_load(self):
+        assertTrue = self.assertTrue
+        fd, ef = Elf.from_zip(self.ZIP_SAMPLE, inmemory=True)
+
+   
