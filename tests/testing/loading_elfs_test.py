ed1d9d139888d4722f72bf4df7659a7e5dfb0152
dso <adam.pridgen.phd@gmail.com>
2020-07-30 22:06:55 -0500

    testing that file loads correctly

--- tests/testing/loading_elfs_test.py
+++ tests/testing/loading_elfs_test.py
@@ -77,6 +77,32 @@ class TestManager(unittest.TestCase):
         x = fd.read(8)
         self.assertTrue(x == b'\x7fELF\x02\x01\x01\x00')
 
+    def test_elf_parsing(self):
+        # from file is really slow, analysis needs to be
+        # in memory, or prepare to sit for a lot longer
+        assertTrue = self.assertTrue
+        fd, ef = Elf.from_zip(self.ZIP_SAMPLE, inmemory=True)
+
+        sections = [i for i in ef.iter_sections()]
+        assertTrue(len(sections) == 132)
+        assertTrue(sorted({i.header.sh_type for i in sections}) == ['SHT_NOTE', 'SHT_NULL', 'SHT_PROGBITS', 'SHT_STRTAB'])
+
+        segments = [i for i in ef.iter_segments()]
+        assertTrue(len(segments) == 130)
+        assertTrue(sorted({i.header.p_type for i in segments}) == ['PT_LOAD', 'PT_NOTE'])
+
+        pt_loads = [i for i in segments if i.header.p_type == 'PT_LOAD']
+        assertTrue(len(pt_loads) == 129)
+        pt_notes = [i for i in segments if i.header.p_type == 'PT_NOTE']
+        pt_note = pt_notes[0]
+        notes = [n for n in pt_notes[0].iter_notes()]
+        assertTrue(len(notes) == 15)
+
+        aux_notes = [n for n in pt_note.iter_notes() if n['n_type'] == 'NT_AUXV']
+        assertTrue(len(aux_notes) == 1)
+        file_notes = [n for n in pt_note.iter_notes() if n['n_type'] == 'NT_FILE']
+        assertTrue(len(file_notes) == 1)
+
 
 if __name__ == '__main__':
     unittest.main()
